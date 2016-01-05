8e4a0e74c9698e628a44c04d10e5a69c54791336
BEZIAT Mayeul <mayeul.beziat@devinci.fr>
2016-01-05 09:11:10 +0100

    Exercice 3
    Complete :)
    Zorghul Power

--- 03-debugging-code/00_Reference_Error/challenge.js
+++ 03-debugging-code/00_Reference_Error/challenge.js
@@ -1,3 +1,9 @@
 // module.exports.hello = function(msg) {
 //   return "Hello " + msg;
 // };
+//module.exports.linkTo = function(text, address) {   
+//  return "<a href='" + address + "'>" + text + "</a>"
+//};
+module.exports.hello = function(msg) {
+  return "Hello " + msg;
+};
